94f959d518d4562c527c7ef4ac949cc1e3fe7445
souuum <hugo.soumbrugevin@gmail.com>
2022-05-17 18:53:24 +0200

    starting Bike Parts Edit

--- VeloMax/MVVM/ViewModel/BikePartsViewModel.cs
+++ VeloMax/MVVM/ViewModel/BikePartsViewModel.cs
@@ -72,8 +72,21 @@ namespace VeloMax.MVVM.ViewModel
             get =>rBikeRow; set
             {
                 rBikeRow = value;
-                OnPropertyChanged(nameof(RBikeRow));
+                OnPropertyChanged();
                 
+            }
+        }
+
+        private string rBikeName;
+
+
+        public string RBikeName
+        {
+            get => rBikeName; set
+            {
+                rBikeName = value;
+                OnPropertyChanged();
+
             }
         }
 
@@ -301,133 +314,23 @@ namespace VeloMax.MVVM.ViewModel
 
             });
 
-            BikeViewRefresh = new RelayCommand(o =>
-            {
-                Console.WriteLine("Refreshing");
-                Console.WriteLine(tName);
-                Console.WriteLine(tType);
-                Console.WriteLine(tStock);
-                if (tName == null)
-                {
-                    Console.WriteLine("PROUUUT");
-                }
-                Console.WriteLine(TName);
-                Console.WriteLine(RBikeRow);
-                InitData();
-            });
-
-            BikeAdd = new RelayCommand(o =>
-            {
-                List<string> cols = new List<string>() { "bike_name", "size", "price", "type", "date_start", "date_end", "stock" };
-                MySqlParameter name = new MySqlParameter("@name", MySqlDbType.VarChar);
-                name.Value = tName;
-                MySqlParameter size = new MySqlParameter("@size", MySqlDbType.VarChar);
-                size.Value = tSize;
-                MySqlParameter price = new MySqlParameter("@price", MySqlDbType.Float);
-                price.Value = float.Parse(tPrice);
-                MySqlParameter type = new MySqlParameter("@type", MySqlDbType.VarChar);
-                type.Value = tType;
-                MySqlParameter dates = new MySqlParameter("@dates", MySqlDbType.Date);
-                dates.Value = new DateTime(Convert.ToInt32(tDateS.Split('/')[2]), Convert.ToInt32(tDateS.Split('/')[1]), Convert.ToInt32(tDateS.Split('/')[0]));
-                MySqlParameter datee = new MySqlParameter("@datee", MySqlDbType.Date);
-                datee.Value = new DateTime(Convert.ToInt32(tDateE.Split('/')[2]), Convert.ToInt32(tDateE.Split('/')[1]), Convert.ToInt32(tDateE.Split('/')[0]));
-                MySqlParameter stock = new MySqlParameter("@stock", MySqlDbType.Int32);
-                stock.Value = Convert.ToInt32(tStock);
-                List<MySqlParameter>  BikeAddData = new List<MySqlParameter>();
-                BikeAddData.Add(name);
-                BikeAddData.Add(size);
-                BikeAddData.Add(price);
-                BikeAddData.Add(type);
-                BikeAddData.Add(dates);
-                BikeAddData.Add(datee);
-                BikeAddData.Add(stock);
-
-                Db.InsertRow("Bikes",cols,BikeAddData);
-
-            });
-
-            BikeDelete = new RelayCommand(o =>
-            {
-                
-                string id = RBikeRow.Split()[0];
-                Db.DeleteDependencies("Bike_Parts", "bike_id", id, false);
-                Db.DeleteDependencies("Bike_Ordered", "bike_id", id, false);
-                Db.DeleteRow("Bikes","bike_id", id, false);
-                
-                Console.WriteLine(RBikeRow);
-
-            });
-
-            BikeUpdate = new RelayCommand(o =>
-            {
-                List<string> cols = new List<string>();
-                string id = RBikeRow.Split()[0];
-                Console.WriteLine(RBikeRow);
-                List<MySqlParameter> BikeUpdateData = new List<MySqlParameter>();
-                if (tName != null) 
-                {
-                    cols.Add("bike_name");
-                    MySqlParameter name = new MySqlParameter("@name", MySqlDbType.VarChar);
-                    name.Value = tName;
-                    BikeUpdateData.Add(name);
-                }
-                if(tSize != null)
-                {
-                    cols.Add("size");
-                    MySqlParameter size = new MySqlParameter("@size", MySqlDbType.VarChar);
-                    size.Value = tSize;
-                    BikeUpdateData.Add(size);
-                }
-                if(tPrice != null)
-                {
-                    cols.Add("price");
-                    MySqlParameter price = new MySqlParameter("@price", MySqlDbType.Float);
-                    price.Value = float.Parse(tPrice);
-                    BikeUpdateData.Add(price);
-                }
-                if(tType != null)
-                {
-                    cols.Add("type");
-                    MySqlParameter type = new MySqlParameter("@type", MySqlDbType.VarChar);
-                    type.Value = tType;
-                    BikeUpdateData.Add(type);
-                }
-                if(tDateS != null)
-                {
-                    cols.Add("date_start");
-                    MySqlParameter dates = new MySqlParameter("@dates", MySqlDbType.Date);
-                    dates.Value = new DateTime(Convert.ToInt32(tDateS.Split('/')[2]), Convert.ToInt32(tDateS.Split('/')[1]), Convert.ToInt32(tDateS.Split('/')[0]));
-                    BikeUpdateData.Add(dates);
-                }
-                if (tDateE != null)
-                {
-                    cols.Add("date_end");
-                    MySqlParameter datee = new MySqlParameter("@datee", MySqlDbType.Date);
-                    datee.Value = new DateTime(Convert.ToInt32(tDateE.Split('/')[2]), Convert.ToInt32(tDateE.Split('/')[1]), Convert.ToInt32(tDateE.Split('/')[0]));
-                    BikeUpdateData.Add(datee);
-                }
-                if(tStock != null)
-                {
-                    cols.Add("stock");
-                    MySqlParameter stock = new MySqlParameter("@stock", MySqlDbType.Int32);
-                    stock.Value = Convert.ToInt32(tStock);
-                    BikeUpdateData.Add(stock);
-                }
-
-                Db.UpdateRow("Bikes", "bike_id", id, false, cols, BikeUpdateData);
-
-            });
+           
 
         }
 
         
         public void InitData()
         {
+            Console.WriteLine("init");
             Console.WriteLine(RBikeRow);
             Console.WriteLine(rBikeRow);
+            RBikeName = rBikeRow;
+            Console.WriteLine(RBikeName);
         }
         public void InitCData()
         {
+            RBikeName = rBikeRow.Split()[0];
+            Console.WriteLine(RBikeName);
             Console.WriteLine("prout "+ RBikeRow + " prout");
             List<string> cols = new List<string>() { "Parts.parts_id","label", "price", "date_start", "date_end", "delay", "Parts.quantity", "part_type" };
 
